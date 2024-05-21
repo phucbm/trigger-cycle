8136d10baae5a883852563ca1bace08d0f046cd8
phucbm <hiphucbui@gmail.com>
2024-05-21 13:53:14 +0700

    fix:build script

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -11,7 +11,7 @@ const path = require("path");
  * Sample variables: "cross-env ENTRY=web"
  * ENTRY: folder to start building the bundle
  */
-const entryFolder = 'site';
+const entryFolder = 'dev';
 const entryPath = path.resolve(__dirname, `../${entryFolder}`);
 
 
@@ -25,7 +25,7 @@ module.exports = merge(server, {
     devtool: false,
 
     // Where webpack looks to start building the bundle
-    entry: [entryPath + '/index.js'],
+    entry: [entryPath + '/script.js'],
 
     output: {
         path: paths.build,
